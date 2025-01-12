84789539974acbd48e684fd42d850d5ae2dee139
lanchuhuong <huonglanchu0712@gmail.com>
2025-01-12 19:44:22 +0100

    update cohen-kappy-score

--- notebooks/utils/utils.py
+++ notebooks/utils/utils.py
@@ -3,8 +3,7 @@ import pandas as pd
 
 
 def get_evaluation_predictions(
-    weave_client: weave.trace.weave_client.WeaveClient, 
-    eval_call_id: str
+    weave_client: weave.trace.weave_client.WeaveClient, eval_call_id: str
 ) -> pd.DataFrame:
     """
     Retrieves evaluation predictions from a Weave call and returns them as a DataFrame.
@@ -20,7 +19,10 @@ def get_evaluation_predictions(
 
     predictions = []
     for eval_call in eval_calls.children():
-        if eval_call.op_name.split("/")[-1].split(":")[0] == "Evaluation.predict_and_score":
+        if (
+            eval_call.op_name.split("/")[-1].split(":")[0]
+            == "Evaluation.predict_and_score"
+        ):
             _eval_call = weave_client.get_call(eval_call.id)
             data = dict(_eval_call.inputs["example"])
             data.update({"pred_score": dict(_eval_call.output)["output"]["score"]})
@@ -43,7 +45,7 @@ def calculate_cohen_kappa(df: pd.DataFrame, labels: list) -> float:
     Raises:
         AssertionError: If required columns 'score' or 'pred_score' are missing from DataFrame
     """
-    required_cols = ['score', 'pred_score']
+    required_cols = ["scores", "pred_score"]
     missing_cols = [col for col in required_cols if col not in df.columns]
 
     assert len(missing_cols) == 0, (
@@ -52,18 +54,14 @@ def calculate_cohen_kappa(df: pd.DataFrame, labels: list) -> float:
     )
 
     if isinstance(df["pred_score"][0], str):
-        score = df['score'].apply(lambda x: 'Bad' if 1 <= x <= 3 else 'Excellent')
-        labels = ['Bad', 'Excellent']
+        score = df["score"].apply(lambda x: "Bad" if 1 <= x <= 3 else "Excellent")
+        labels = ["Bad", "Excellent"]
     else:
         score = df["score"]
 
     from sklearn.metrics import cohen_kappa_score
-    return cohen_kappa_score(
-        score,
-        df['pred_score'],
-        labels=labels,
-        weights='linear'
-    )
+
+    return cohen_kappa_score(score, df["pred_score"], labels=labels, weights="linear")
 
 
 def calculate_kendall_tau(df: pd.DataFrame) -> float:
@@ -80,7 +78,7 @@ def calculate_kendall_tau(df: pd.DataFrame) -> float:
     Raises:
         AssertionError: If required columns 'score' or 'pred_score' are missing from DataFrame
     """
-    required_cols = ['score', 'pred_score']
+    required_cols = ["score", "pred_score"]
     missing_cols = [col for col in required_cols if col not in df.columns]
 
     assert len(missing_cols) == 0, (
@@ -89,7 +87,5 @@ def calculate_kendall_tau(df: pd.DataFrame) -> float:
     )
 
     from scipy.stats import kendalltau
-    return kendalltau(
-        df["score"],
-        df['pred_score']
-    ).statistic
+
+    return kendalltau(df["score"], df["pred_score"]).statistic
